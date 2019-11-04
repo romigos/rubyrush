69c72f63cfab1e77745016bcd6551be6a6390e9d
bolistovskiyroman@gmail.com <RemkA1980>
2019-11-04 14:54:20 +0200

    kum 3 some fix

--- array-22.rb
+++ array-22.rb
@@ -1,9 +1,21 @@
-#* Мужчины
+# Мужчины
 #["Gena", "George", "Misha"]
-#Женщины
+# Женщины
 #["Katrin", "Liza", "Masha"]
 
+#заводимо масив чоловіків
 array1 = ["Gena", "George", "Misha"]
+puts 'Чоловіки'
 puts array1.to_s
+
+# заводимо масив жінок
 array2 = ["Katrin", "Liza", "Masha"]
+puts 'Жінки'
 puts array2.to_s
+
+# додаємо масиви
+array3 = array1 + array2
+
+# виводимо результат на еран
+puts 'Всі разом'
+puts array3.to_s
