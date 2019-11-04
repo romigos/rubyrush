4d6b3817d47aebcf8ee3c3f4e8172b2145b2288e
bolistovskiyroman@gmail.com <RemkA1980>
2019-11-04 11:10:11 +0200

    kum 3

--- kum2.rb
+++ kum2.rb
@@ -2,14 +2,16 @@
 # 1-19 період1
 # 15-33 період2
 # 34-40 період3
-res = 18
+res = 1
+periods = []
 # Робимо зрівняння
 if res >= 1 && res <= 19
-  puts 'Період 1'
+  periods.push ['Період 1']
 end
 if res >= 15 && res <= 33
-  puts 'Період 2'
+  periods.push ['Період 2']
 end
 if res >= 34 && res <= 40
-  puts 'Період 3'
+  periods.push ['Період 3']
 end
+puts periods.to_s
