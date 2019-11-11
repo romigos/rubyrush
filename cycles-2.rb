425a6f5c0fcb1096e8107e5e1b58f09d3207d4e9
romigod <bolistovskiyroman@gmail.com>
2019-11-11 23:46:53 +0200

    program 27

--- cycles-2.rb
+++ cycles-2.rb
@@ -1,2 +1,24 @@
 names = []
 user_input = nil
+
+while user_input != '' do
+  user_input = gets.encode('UTF-8').chomp
+  names << user_input
+end
+
+for item in names do
+  puts ' С нами ' + item
+  sleep 2
+
+  if (item == 'Элис')
+    puts 'Элис? А кто такая Элис?'
+    sleep 1
+    break
+  end
+end
+
+puts 'А что это за девочка и где она живет'
+sleep 2
+puts 'А вдруг она не курит? А вдруг она не пьет?'
+sleep 2
+puts 'Ну а ми с такими рожами возьмем и да и припремся к Элис ...'
