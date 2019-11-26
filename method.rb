f9fc960da3bd80dab1d0a0d0fb5026dd8f494fca
romigod <bolistovskiyroman@gmail.com>
2019-11-27 01:14:11 +0200

    program 34 part 2

--- method.rb
+++ method.rb
@@ -3,3 +3,23 @@ def say_hi(name)
 end
 
 say_hi('Роман')
+
+def sklonenie(number, krakodil, krokodila, krokodilov)
+  ostatok = number % 10
+
+  if ostatok == 1
+    return krakodil
+  end
+  if ostatok >= 2 && otatok <= 4
+    return krokodila
+  end
+  if ostatok > 4 || ostatok == 0
+    return krokodilov
+  end
+end
+
+skolko = ARGV[0].to_i
+puts "#{skolko} " +
+         sklonenie(skolko, 'негритенок', 'негритенка', 'негритят') + " " +
+         sklonenie(skolko, 'пошел', 'пошли', 'пошли') + " " +
+         'купатся в море!'
